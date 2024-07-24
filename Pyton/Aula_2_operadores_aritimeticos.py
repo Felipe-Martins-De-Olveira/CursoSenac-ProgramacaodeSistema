14035ec14342ba60647dcf97ca23b8846208ab23
Felipe-Martins-De-Olveira <160751172+Felipe-Martins-De-Olveira@users.noreply.github.com>
2024-07-23 21:44:49 -0300

    Create Aula_2_operadores_aritimeticos.py

---
+++ Pyton/Aula_2_operadores_aritimeticos.py
@@ -0,0 +1,29 @@
+ #Aula
+a = 1
+
+b = 10
+
+soma = a + b
+
+#soma +
+#subtrair -
+#dividir  /
+#multiplicação *
+
+print(soma)
+
+#Atividade
+
+#soma
+soma = 1 + 2
+
+#subtrair
+subtrair = 1 - 2
+
+#dividir
+dividir = 2 / 2
+
+#multiplicar
+multiplicar = 2 * 2
+
+print(f" A soma é: {soma} A subtração é: {subtrair} A divisão é: {dividir} A multiplicação é: {multiplicar}")
