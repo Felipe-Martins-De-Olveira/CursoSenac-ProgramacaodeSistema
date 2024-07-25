932987a96f69031fd5f4226e00f7139b125080a1
Felipe-Martins-De-Olveira <160751172+Felipe-Martins-De-Olveira@users.noreply.github.com>
2024-07-24 21:48:55 -0300

    Aula03

---
+++ Python/Algoritmo/Aula_3_funcao.py
@@ -0,0 +1,22 @@
+# Aula
+ numero_1 = "Senac "
+ numero_2 = "bom de mais"
+ 
+ numero_3 =  numero_1 + numero_2
+ 
+ print(numero_3)
+
+ numero_1 = int(input("Diga o primeiro número: "))
+ numero_2 = int(input("Digite o segundo número: "))
+
+ soma = numero_1 + numero_2
+ 
+ print(soma)
+
+# Atividade
+n1 = int(input("Digite o primeiro número: " ))
+n2 = int(input("Digite o segundo número: "))
+
+resultado = n1 + n2
+
+print(resultado)
